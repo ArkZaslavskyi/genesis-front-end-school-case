644a608105664c406224c0374d375c7bd0831eeb
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2023-03-21 17:43:55 +0200

    add CoursePage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { CoursePage } from "pages/CoursePage";
 import { Courses } from "pages/Courses";
 import { Route, Routes } from "react-router-dom";
 import GlobalStyle from "theme/Globalstyle";
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<Layout />} >
         {/* ... path="/" element={<Layout />} */}
           <Route path="/courses" element={<Courses />} />
-          <Route path="/courses/:courseId" />
+          <Route path="/courses/:courseId" element={<CoursePage />} />
         </Route>
       </Routes>
     </>
---
+++ src/pages/CoursePage.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getCourse, getToken } from "services/Api";
+
+export const CoursePage = () => {
+  const [course, setCourse] = useState(null);
+  const { courseId } = useParams();
+
+  useEffect(() => {
+    async function fetchData() {
+      await getToken();
+      await getCourse(courseId).then((res) => setCourse(res.data));
+    }
+
+    fetchData();
+  }, [courseId]);
+
+  if (!course) return;
+  console.log(course);
+  return <>{/* <CoursesList courses={courses} /> */}</>;
+};
