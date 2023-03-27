558ea0600c6b8297ae65f7119dbe875bfc1eb659
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2023-03-27 03:17:50 +0300

    correct CourseCard layout & styles

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -8,13 +8,17 @@ export const CourseCard = ({ course }) => {
 
   return (
     <li className={css.courseCard}>
-      <h2><NavLink className={css.courseLink} to={`/courses/${id}`}>{title}</NavLink></h2>
-      {courseVideoPreview?.link &&
-        <Player link={courseVideoPreview.link} preview={previewImageLink + '/cover.webp'} controls={false} />}
-      <p><span className={css.courseTag}>Total lessons:</span> {lessonsCount}</p>
-      {meta?.skills?.length &&
-        <p><span className={css.courseTag}>Skills:</span> {course.meta.skills.join(' | ')}</p>}
-      <p><span className={css.courseTag}>Rating:</span> { rating }</p>
+      <h2 className={css.noMargin}><NavLink className={css.courseLink} to={`/courses/${id}`}>{title}</NavLink></h2>
+      <div className={css.playerWrapper}>
+        {courseVideoPreview?.link &&
+          <Player link={courseVideoPreview.link} preview={previewImageLink + '/cover.webp'} controls={false} />}
+      </div>
+      <div className={css.textWrapper}>
+        <p className={css.tagBlock}><span className={css.courseTag}>Total lessons:</span> {lessonsCount}</p>
+        {meta?.skills?.length &&
+          <p className={css.tagBlock}><span className={css.courseTag}>Skills:</span> {course.meta.skills.join(' | ')}</p>}
+        <p className={css.tagBlock}><span className={css.courseTag}>Rating:</span> {rating}</p>
+      </div>
     </li>
   )
 };
